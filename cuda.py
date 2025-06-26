b812c567f8bb785d36dc2a91495798a8e8189af9
Utkarsh Pawade <utkarshpawadecse@gmail.com>
2025-06-26 22:52:33 +0530

    Create cuda.py

---
+++ cuda.py
@@ -0,0 +1,10 @@
+import copy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+to8b = lambda x : (255*np.clip(x.cpu().numpy(),0,1)).astype(np.uint8)
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+    TENSORBOARD_FOUND = True
+except ImportError:
+    TENSORBOARD_FOUND = False
